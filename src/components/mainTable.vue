120102bf02a84cd3de34c8f69e721f2910a096d9
DoReVo <frz565@gmail.com>
2019-01-23 10:18:36 +0800

    Done issue #2

--- src/components/mainTable.vue
+++ src/components/mainTable.vue
@@ -19,96 +19,97 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       fields: [
-        { key: 'day', label: 'Day', thClass: 'align-middle', sortable: false },
+        { key: "day", label: "Day", thClass: "align-middle", sortable: false },
         {
-          key: 'date',
-          label: 'Date',
-          thClass: 'align-middle',
+          key: "date",
+          label: "Date",
+          thClass: "align-middle",
           sortable: false
         },
         {
-          key: 'biActivities',
-          label: 'B&I Activities',
-          thClass: 'align-middle'
+          key: "biActivities",
+          label: "B&I Activities",
+          thClass: "align-middle"
         },
         {
-          key: 'permitToWork',
-          label: 'Permit To Work',
-          thClass: 'align-middle'
+          key: "permitToWork",
+          label: "Permit To Work",
+          thClass: "align-middle"
         },
         {
-          key: 'projectActivities',
-          label: 'Project Activities',
-          thClass: 'align-middle'
+          key: "projectActivities",
+          label: "Project Activities",
+          thClass: "align-middle"
         },
-        { key: '1', label: '1' },
-        { key: '2', label: '2' },
-        { key: '3', label: '3' },
-        { key: '4', label: '4' },
-        { key: '5', label: '5' },
-        { key: '6', label: '6' },
-        { key: '7', label: '7' },
-        { key: '8', label: '8' }
+        { key: "1", label: "1" },
+        { key: "2", label: "2" },
+        { key: "3", label: "3" },
+        { key: "4", label: "4" },
+        { key: "5", label: "5" },
+        { key: "6", label: "6" },
+        { key: "7", label: "7" },
+        { key: "8", label: "8" }
         // { key: "contractManagement", label: "Contract Management" },
         // { key: "contractorManagement", label: "Contractor Management" }
       ],
       items: [],
-      init: false
-    }
+      init: false,
+      currentExpandedRow: null
+    };
   },
   methods: {
-    getDays: function (year, month) {
-      let date = new Date(year, month, 1)
-      let days = []
+    getDays: function(year, month) {
+      let date = new Date(year, month, 1);
+      let days = [];
       while (date.getMonth() == month) {
         days.push({
-          day: date.toLocaleString('en-gb', { weekday: 'short' }),
+          day: date.toLocaleString("en-gb", { weekday: "short" }),
           date: date.getDate()
-        })
-        date.setDate(date.getDate() + 1)
+        });
+        date.setDate(date.getDate() + 1);
       }
-      return days
+      return days;
     },
-    modifyTable: function () {
-      var thead = document.querySelector('thead')
-      var newRow = document.createElement('tr')
-      thead.append(newRow)
-      var toBeCloneTh = document.querySelectorAll('th')
-      var newTh = []
+    modifyTable: function() {
+      var thead = document.querySelector("thead");
+      var newRow = document.createElement("tr");
+      thead.append(newRow);
+      var toBeCloneTh = document.querySelectorAll("th");
+      var newTh = [];
       for (let i of toBeCloneTh) {
         switch (i.innerHTML) {
-          case '1':
-          case '2':
-          case '3':
-          case '4':
-          case '5':
-          case '6':
-          case '7':
-          case '8':
-            newTh.push(i)
-            break
+          case "1":
+          case "2":
+          case "3":
+          case "4":
+          case "5":
+          case "6":
+          case "7":
+          case "8":
+            newTh.push(i);
+            break;
           default:
-            break
+            break;
         }
       }
-      let cM = document.createElement('th')
-      cM.innerHTML = 'Contract Management'
-      document.querySelector('thead tr:nth-child(1)').appendChild(cM)
+      let cM = document.createElement("th");
+      cM.innerHTML = "Contract Management";
+      document.querySelector("thead tr:nth-child(1)").appendChild(cM);
 
-      newRow = document.querySelector('thead tr:nth-child(2)')
+      newRow = document.querySelector("thead tr:nth-child(2)");
 
       // Append to 2nd row
       for (let i of newTh) {
-        newRow.appendChild(i)
+        newRow.appendChild(i);
       }
       this.fields.push({
-        key: 'contractorManagement',
-        label: 'Contractor Management',
-        thClass: 'align-middle'
-      })
+        key: "contractorManagement",
+        label: "Contractor Management",
+        thClass: "align-middle"
+      });
       // let firstRow = document.querySelectorAll("thead tr:nth-child(1) th");
 
       // for (let i of firstRow) {
@@ -122,64 +123,69 @@ export default {
       // }
       // console.log(firstRow);
     },
-    modifyHeader: function () {
-      let firstRow = document.querySelectorAll('thead tr:nth-child(1) th')
+    modifyHeader: function() {
+      let firstRow = document.querySelectorAll("thead tr:nth-child(1) th");
       for (let i of firstRow) {
         switch (i.innerHTML) {
-          case 'Contract Management':
-            i.setAttribute('colspan', '8')
-            break
+          case "Contract Management":
+            i.setAttribute("colspan", "8");
+            break;
           default:
             // i.classList.add("align-middle");
-            i.setAttribute('rowspan', '2')
+            i.setAttribute("rowspan", "2");
         }
       }
-      this.init = true
+      this.init = true;
     },
-    rowClicked: function (item, index, event) {
+    rowClicked: function(item, index, event) {
+      this.currentExpandedRow != null &&
+      this.currentExpandedRow._showDetails == true
+        ? (this.currentExpandedRow._showDetails = false)
+        : "";
       // Expand row details
-      item._showDetails = !item._showDetails
+      item._showDetails = !item._showDetails;
+      this.currentExpandedRow = item;
       // Wait for DOM to re-render
-      this.$nextTick(function () {
+      this.$nextTick(function() {
         if (item._showDetails) {
-          var detailRow = event.target.parentNode.nextSibling
-          console.log(detailRow)
+          var detailRow = event.target.parentNode.nextSibling;
+          console.log(detailRow);
         }
-      })
+      });
     }
   },
-  created: function () {
-    let days = this.getDays(2019, 0)
+  created: function() {
+    let days = this.getDays(2019, 0);
     for (let day of days) {
       this.items.push({
         day: day.day,
         date: day.date,
-        biActivities: '',
-        permitToWork: '',
-        projectActivities: '',
-        1: '',
-        2: '',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        8: '',
-        contractorManagement: '',
+        biActivities: "",
+        permitToWork: "",
+        projectActivities: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        contractorManagement: "",
         _showDetails: false
-      })
+      });
     }
   },
-  mounted: function () {
+  mounted: function() {
     // Wait for all to render the modifyTable()
-    this.$nextTick(function () {
-      this.modifyTable()
-    })
+    this.$nextTick(function() {
+      this.modifyTable();
+    });
   },
-  updated: function () {
-    this.init == false ? this.modifyHeader() : ''
+  updated: function() {
+    this.init == false ? this.modifyHeader() : "";
   }
-}
+};
 </script>
 
 <style scoped>
