e1c4afd2914da78e882023613db89e3090fdeb3d
DoReVo <DoReVo@github.com>
2019-01-29 22:28:16 +0800

    Added mainTable getter mutations and actions

--- src/store.js
+++ src/store.js
@@ -1,59 +1,58 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     childTable: {
       biA: {
-        tableName: "biA",
-        fields: { fmNo: "FM NO", activities: "Activities", buttons: "Actions" },
-        items: [
-          { fmNo: "2", activities: "What the DDD" },
-          { fmNo: "4", activities: "DUDE" },
-          { fmNo: "7", activities: "Weed" }
-        ]
+        tableName: 'biA',
+        fields: { fmNo: 'FM NO', activities: 'Activities', buttons: 'Actions' },
+        items: [{ fmNo: '2', activities: 'What the DDD' }, { fmNo: '4', activities: 'DUDE' }, { fmNo: '7', activities: 'Weed' }]
       },
       ptw: {
-        tableName: "ptw",
-        fields: { fmNo: "FM NO", activities: "Activities", buttons: "Actions" },
-        items: [
-          { fmNo: "2", activities: "What the DDD" },
-          { fmNo: "4", activities: "DUDE" },
-          { fmNo: "777", activities: "TowTruck" }
-        ]
+        tableName: 'ptw',
+        fields: { fmNo: 'FM NO', activities: 'Activities', buttons: 'Actions' },
+        items: [{ fmNo: '2', activities: 'What the DDD' }, { fmNo: '4', activities: 'DUDE' }, { fmNo: '777', activities: 'TowTruck' }]
       },
       pa: {
-        tableName: "pa",
-        fields: { fmNo: "FM NO", activities: "Activities" },
-        items: [
-          { fmNo: "2", activities: "What the DDD" },
-          { fmNo: "4", activities: "DUDE" }
-        ]
+        tableName: 'pa',
+        fields: { fmNo: 'FM NO', activities: 'Activities' },
+        items: [{ fmNo: '2', activities: 'What the DDD' }, { fmNo: '4', activities: 'DUDE' }]
       }
-    }
+    },
+    mainTableData: []
   },
   getters: {
     biA: state => {
-      return state.childTable.biA;
+      return state.childTable.biA
     },
     ptw: state => {
-      return state.childTable.ptw;
+      return state.childTable.ptw
     },
     pa: state => {
-      return state.childTable.pa;
+      return state.childTable.pa
+    },
+    mainTableData: state => {
+      return state.mainTableData
     }
   },
   mutations: {
     addRow: (state, payload) => {
-      console.log(payload);
-      state.childTable[payload.table].items.push(payload["data"]);
+      // console.log(payload);
+      state.childTable[payload.table].items.push(payload['data'])
+    },
+    mainTableAddRow: (state, payload) => {
+      state.mainTableData.push(payload)
     }
   },
   actions: {
     addRow: (context, payload) => {
-      context.commit("addRow", payload);
+      context.commit('addRow', payload)
+    },
+    mainTableAddRow: (context, payload) => {
+      context.commit('mainTableAddRow', payload)
     }
   }
-});
+})
