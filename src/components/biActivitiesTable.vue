3fef20f2562dcb3709e507f520e4923738133465
DoReVo <frz565@gmail.com>
2019-01-29 17:22:56 +0800

    1.Modified table to print passed props.

--- src/components/biActivitiesTable.vue
+++ src/components/biActivitiesTable.vue
@@ -1,55 +1,39 @@
 <template>
-  <b-table class="m-0" :fields="fields" :items="items">
-    <template slot="table-caption">
-      <b-button @click.stop="addRow" size="sm" class="rounded-circle">+</b-button>
-    </template>
-    <template slot="actionCol" slot-scope="data">
-      <b-button size="sm" class="rounded-circle">Edit</b-button>
-      <b-button size="sm" class="rounded-circle">Delete</b-button>
-    </template>
-  </b-table>
+  <table>
+    <thead>
+      <th :key="key" v-for="(value,key) in fields">{{value}}</th>
+    </thead>
+    <tbody>
+      <tr :key="rowKey" v-for="(value,rowKey) in items">
+        <td :key="key" v-for="(tdVal,key) in items[rowKey]">{{tdVal}}</td>
+        <td>
+          <input type="button" value="*">
+          <input type="button" value="-">
+        </td>
+      </tr>
+      <input type="button" value="+" @click.stop="addRow">
+    </tbody>
+  </table>
 </template>
+
 
 <script>
 export default {
+  props: { fields: Object, items: Array, tableName: String },
+
   data() {
     return {
-      fields: [
-        {
-          key: "fmNo",
-          label: "#FM",
-          thClass: "align-middle"
-        },
-        {
-          key: "activity",
-          label: "Activity",
-          thClass: "align-middle"
-        },
-        {
-          key: "actionCol",
-          label: "Action",
-          thClass: "align-middle"
-        }
-      ],
-      items: [
-        { fmNo: "23", activity: "Jack Ryan" },
-        { fmNo: "23", activity: "Police story 1" },
-        { fmNo: "23", activity: "Police story 2" },
-        { fmNo: "W", activity: "Men in black" }
-      ]
+      // tableName: this.tableName
     };
   },
   methods: {
     addRow: function() {
-      this.items.push({
-        fmNo: "420",
-        activity: "R O F L"
-      });
+      let newRow = {
+        table: this.tableName,
+        data: { fmNo: "420", activity: "R O F L" }
+      };
+      this.$store.dispatch("addRow", newRow);
     }
   }
 };
 </script>
-
-
-<style scoped>
-</style>
