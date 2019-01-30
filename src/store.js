5efc522860d17b97452fceeff6f5cd090d0ae569
DoReVo <DoReVo@github.com>
2019-01-31 00:08:09 +0800

    1. Remove simulateComplete state. 2. Changed addrow() to find element that matches payload date,month,year and then mutate that resulted object 3. Changed toggleRowDetails() to find element that matches payload date,month,year and then mutate that resulted object

--- src/store.js
+++ src/store.js
@@ -1,173 +1,77 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    simulatedComplete: [
-      {
-        month: "Apr",
-        year: "2018",
-        mainTableData: [
-          {
-            mainDetails: {
-              day: "Fri",
-              date: "2",
-              biActivities: "",
-              permitToWork: "",
-              projectActivities: "",
-              n1: "",
-              n2: "",
-              n3: "",
-              n4: "",
-              n5: "",
-              n6: "",
-              n7: "",
-              n8: "",
-              contractorManagement: ""
-            },
-            childTable: {
-              biA: {
-                tableName: "biA",
-                fields: {
-                  fmNo: "FM NO",
-                  activities: "Activities",
-                  buttons: "Actions"
-                },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" },
-                  { fmNo: "7", activities: "Weed" }
-                ]
-              },
-              ptw: {
-                tableName: "ptw",
-                fields: {
-                  fmNo: "FM NO",
-                  activities: "Activities",
-                  buttons: "Actions"
-                },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" },
-                  { fmNo: "777", activities: "TowTruck" }
-                ]
-              },
-              pa: {
-                tableName: "pa",
-                fields: { fmNo: "FM NO", activities: "Activities" },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" }
-                ]
-              }
-            },
-            rowDetails: false
-          },
-          {
-            mainDetails: {
-              day: "Fri",
-              date: "2",
-              biActivities: "",
-              permitToWork: "",
-              projectActivities: "",
-              n1: "",
-              n2: "",
-              n3: "",
-              n4: "",
-              n5: "",
-              n6: "",
-              n7: "",
-              n8: "",
-              contractorManagement: ""
-            },
-            childTable: {
-              biA: {
-                tableName: "biA",
-                fields: {
-                  fmNo: "FM NO",
-                  activities: "Activities",
-                  buttons: "Actions"
-                },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" },
-                  { fmNo: "7", activities: "Weed" }
-                ]
-              },
-              ptw: {
-                tableName: "ptw",
-                fields: {
-                  fmNo: "FM NO",
-                  activities: "Activities",
-                  buttons: "Actions"
-                },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" },
-                  { fmNo: "777", activities: "TowTruck" }
-                ]
-              },
-              pa: {
-                tableName: "pa",
-                fields: { fmNo: "FM NO", activities: "Activities" },
-                items: [
-                  { fmNo: "2", activities: "What the DDD" },
-                  { fmNo: "4", activities: "DUDE" }
-                ]
-              }
-            },
-            rowDetails: false
-          }
-        ]
-      }
-    ],
     realSimulationTableData: []
   },
   getters: {
     realSimulationTableData: state => {
-      return state.realSimulationTableData;
+      return state.realSimulationTableData
     },
     simulatedCompleteData: state => {
-      return state.simulatedComplete;
+      return state.simulatedComplete
     },
     currentExpandedRow: state => {
       return state.realSimulationTableData.filter(element => {
-        return element.mainTableData.rowDetails == true;
-      });
+        return element.mainTableData.rowDetails == true
+      })
     }
   },
   mutations: {
     addRow: (state, payload) => {
+      // console.log(payload)
+      let resultData = state.realSimulationTableData.find(element => {
+        return (
+          element.year == payload.rowData.year &&
+          element.month == payload.rowData.month &&
+          element.mainTableData.mainDetails.date == payload.rowData.mainTableData.mainDetails.date
+        )
+      })
+      // console.log(resultData)
+      resultData.mainTableData.childTable[payload.table].items.push(payload.data)
       // console.log(payload);
-      state.realSimulationTableData[payload.rowId].mainTableData.childTable[
-        payload.table
-      ].items.push(payload["data"]);
+      // state.realSimulationTableData[payload.rowId].mainTableData.childTable[payload.table].items.push(payload['data'])
     },
     mainTableAddRow: (state, payload) => {
-      state.mainTableData.push(payload);
+      state.mainTableData.push(payload)
     },
     realSimulationTableDataInit: (state, payload) => {
-      state.realSimulationTableData.push(payload);
+      state.realSimulationTableData.push(payload)
     },
     toggleRowDetails: (state, payload) => {
-      state.realSimulationTableData[payload].mainTableData.rowDetails = !state
-        .realSimulationTableData[payload].mainTableData.rowDetails;
+      let resultData = state.realSimulationTableData.find(element => {
+        return (
+          element.year == payload.year &&
+          element.month == payload.month &&
+          element.mainTableData.mainDetails.date == payload.mainTableData.mainDetails.date
+        )
+      })
+      // console.log('before invert' + resultData)
+      resultData.mainTableData.rowDetails = !resultData.mainTableData.rowDetails
+      // console.log(resultData)
+    },
+    emptyMainData: (state, payload) => {
+      state.realSimulationTableData = []
     }
   },
   actions: {
     addRow: (context, payload) => {
-      context.commit("addRow", payload);
+      context.commit('addRow', payload)
     },
     mainTableAddRow: (context, payload) => {
-      context.commit("mainTableAddRow", payload);
+      context.commit('mainTableAddRow', payload)
     },
     realSimulationTableDataInit: (context, payload) => {
-      context.commit("realSimulationTableDataInit", payload);
+      context.commit('realSimulationTableDataInit', payload)
     },
     toggleRowDetails: (context, payload) => {
-      context.commit("toggleRowDetails", payload);
+      context.commit('toggleRowDetails', payload)
+    },
+    emptyMainData: (context, payload) => {
+      context.commit('emptyMainData')
     }
   }
-});
+})
