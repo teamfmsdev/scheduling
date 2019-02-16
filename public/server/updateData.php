63f6343a973d6bc9bea827013ce249af8180f439
DoReVo <DoReVo@github.com>
2019-02-17 00:49:23 +0800

    Added operations for childTable to : 1. Create row 2. Edit row 3. Delete Row 4. Revalidate row
    Added maintable operations to:
    1. Changed cm color

--- public/server/updateData.php
+++ public/server/updateData.php
@@ -12,25 +12,80 @@ $row= $data["row"];
 $column = $data["type"];
 $newValue = $data["data"];
 
-if($data["operation"]=="editChildTableData"){
-  $stmt = $con -> prepare("UPDATE `$table''` SET `$column`='$newValue' WHERE `row`=$row");
 
-  // $i=1;
-  // foreach ($data as $key => $value) {
-  //     if($key == "SAP_Choice"){
-  //       continue;
-  //     }else {
-  //       $stmt-> bindValue($i,$value);
-  //       $i++;
-  //     }
-  //   }
+if($data["operation"]=="editChildTableData"){
+  $stmt = $con -> prepare("UPDATE `$table` SET `$column`='$newValue' WHERE `row`='$row'");
+
   if($stmt -> execute()){
       echo "Update sucess";
   }else{
       echo "Update failed";
   }
-}else {
-  // $stmt = $con -> prepare("INSERT INTO `$table`  (`date`,`row`) VALUES ('$date','')");
+}elseif($data["operation"]=="addChildTableData") {
+
+  switch($table){
+    case "bia":
+    case "ptw":
+    $stmt = $con -> prepare("INSERT INTO `$table` (`date`) VALUES ('$date');
+    SELECT LAST_INSERT_ID();");
+    break;
+    case "pa":
+    case "cm":
+    $stmt = $con -> prepare("INSERT INTO `$table` (`date`) VALUES ('$date');
+    SELECT LAST_INSERT_ID();");
+    break;
+    default:
+    break;
+  }
+
+  if($stmt -> execute()){
+    $rowValue = $con ->lastInsertId();
+    $message["row"]=$rowValue;
+    // $message["opsCode"]="1";
+    $message["serverMessage"]="Record Inserted successfully";
+    
+    echo json_encode($message);
+}else{
+    echo "Failed to insert new record";
+}
+  
+}elseif($data["operation"]=="deleteChildTableData"){
+  $stmt = $con -> prepare("DELETE FROM $table WHERE (`row`='$row')");
+  if($stmt->execute()){
+    echo "Record deleted successfully";
+  }else{
+    echo "Failed to delete record";
+  }
+}
+elseif($data["operation"]=="reValidateChildTableData"){
+  $fmNo = $_GET["fmNo"];
+  $activities = $_GET["activities"];
+  $stmt = $con -> prepare("INSERT INTO `$table` (`date`,`fmNo`,`activities`)
+  VALUES ('$date','$fmNo','$activities') ;
+  SELECT LAST_INSERT_ID();");
+
+if($stmt -> execute()){
+  $rowValue = $con ->lastInsertId();
+  $message["row"]=$rowValue;
+  $message["serverMessage"]="Revalidate successfull";
+  
+  echo json_encode($message);
+}else{
+  echo "Failed to revalidate";
+}
+}
+elseif($data["operation"]=="mainTableEditRow"){
+
+    $stmt= $con -> prepare("UPDATE `$table` SET `$column`='$newValue' WHERE `date`='$date'");
+
+    if ($stmt->execute()){
+      $message["serverMessage"] = "Color updated";
+      echo json_encode($message);
+    }
+    else{
+      $message["serverMessage"] = "Color failed to update";
+      echo json_encode($message);
+    }
 }
 
 
