f02ed84a2d63067937d9c6a1421534026d36805d
DoReVo <DoReVo@github.com>
2019-02-17 00:48:04 +0800

    1. Changed data retrieval after month/year change to retrieve table 1 by 1 and append to an array that will be echo

--- public/server/retrieveData.php
+++ public/server/retrieveData.php
@@ -6,47 +6,202 @@ foreach ($_GET as $key => $value) {
   }
 
 $date = $data['date'];
-  $stmt = $con -> prepare("SELECT `parentrow`.`date`,
-  `parentrow`.`n2`,`parentrow`.`n2`,`parentrow`.`n3`,
-  `parentrow`.`n4`,`parentrow`.`n5`,`parentrow`.`n6`,
-  `parentrow`.`n7`,`parentrow`.`n8`,`bia`.`biaRow`, `bia`.`biaFmNo`, `bia`.`biaActivities`,`ptw`.`ptwRow`, `ptw`.`ptwPtwNo`, `ptw`.`ptwActivities`,`ptw`.`ptwRow`,`pa`.`paRow`,`pa`.`paActivities`,`cm`.`cmRow`,`cm`.`cmActivities`
-  FROM `parentrow`
-  INNER JOIN `bia` ON `parentrow`.`date`=`bia`.`biaDate`
-  INNER JOIN `ptw` ON `parentrow`.`date`=`ptw`.`ptwDate`
-  INNER JOIN `pa` ON `parentrow`.`date`=`pa`.`paDate`
-  INNER JOIN `cm` ON `parentrow`.`date`=`cm`.`cmDate`
-  WHERE YEAR(`parentrow`.`date`)=YEAR('$date') AND
-  MONTH(`parentrow`.`date`)=MONTH('$date') ");
 
+  // $stmt = $con -> prepare("SELECT `parentrow`.`date`,
+  // `parentrow`.`n1`, `parentrow`.`n2`,`parentrow`.`n3`,`parentrow`.`n4`,
+  // `parentrow`.`n5`,`parentrow`.`n6`,`parentrow`.`n7`,
+  // `parentrow`.`n8`,`bia`.`biaRow`, `bia`.`biaFmNo`, `bia`.`biaActivities`,`ptw`.`ptwRow`, `ptw`.`ptwPtwNo`, `ptw`.`ptwActivities`,`ptw`.`ptwRow`,`pa`.`paRow`,`pa`.`paActivities`,`cm`.`cmRow`,`cm`.`cmActivities`
+  // FROM `parentrow`
+  // FULL OUTER JOIN `bia` ON `parentrow`.`date`=`bia`.`biaDate`
+  // FULL OUTER JOIN `ptw` ON `parentrow`.`date`=`ptw`.`ptwDate`
+  // FULL OUTER JOIN `pa` ON `parentrow`.`date`=`pa`.`paDate`
+  // FULL OUTER JOIN `cm` ON `parentrow`.`date`=`cm`.`cmDate`
+  // WHERE YEAR(`parentrow`.`date`)=YEAR('$date') AND
+  // MONTH(`parentrow`.`date`)=MONTH('$date') ");
 
+  $parentRowFetch = $con -> prepare("SELECT * FROM `parentRow` WHERE YEAR(`date`)=YEAR('$date') 
+  AND MONTH(`date`)=MONTH('$date')");
+  $jsonData;
+  if($parentRowFetch->execute()){
 
-
-
-if($stmt->execute()){
-  
-  $row = $stmt ->fetchAll(PDO::FETCH_ASSOC);
-  foreach ($row as $key => $value) {
-    $jsonData[$key];
-    foreach($row[$key] as $columnKey => $columnValue){
-      if(strstr($columnKey,"bia")!=false){
-        $objKey = str_replace("bia","",$columnKey);
-        $jsondata[$key][$objKey]=$columnValue;
-      }elseif(strstr($columnKey,"ptw")!=false){
-        $objKey = str_replace("ptw","",$columnKey);
-        $jsondata[$key][$objKey]=$columnValue;
-      }elseif(strstr($columnKey,"pa")!=false){
-        $objKey = str_replace("pa","",$columnKey);
-        $jsondata[$key][$objKey]=$columnValue;
-      }elseif(strstr($columnKey,"cm")!=false){
-        $objKey = str_replace("cm","",$columnKey);
-        $jsondata[$key][$objKey]=$columnValue;
-      }else{
-        $jsondata[$key][$columnKey]=$columnValue;
+    $resultData = $parentRowFetch ->fetchAll(PDO::FETCH_ASSOC);
+    // Numeric array
+    foreach ($resultData as $rowKey => $rowValue) {
+      // Assoc array
+      foreach ($resultData[$rowKey] as $columnKey => $columnValue) {
+        switch($columnKey){
+          case "date":
+          // date assoc for comparison 
+          $jsonData[$rowKey]["date"] = $columnValue;
+          // Standard data structure
+          $date = strtotime($columnValue);
+          $jsonData[$rowKey]['month'] = date("M",$date);
+          $jsonData[$rowKey]['year'] = date("Y",$date);
+          $jsonData[$rowKey]["mainTable"]['day'] = date("D",$date);
+          $jsonData[$rowKey]["mainTable"]['date'] = date("d",$date);
+          break;
+          default:
+          $jsonData[$rowKey]["mainTable"][$columnKey] = $columnValue;
+        }
       }
     }
-
   }
+
+  $tempArray=[];
+  $date = $data["date"];
+  $biaFetch = $con -> prepare("SELECT * FROM `bia` 
+  WHERE YEAR(`date`)=YEAR('$date') AND MONTH(`date`)=MONTH('$date')") ;
+
+  if($biaFetch->execute()){
+    $resultData = $biaFetch -> fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($resultData as $rowKey => $rowValue) {
+      foreach ($resultData[$rowKey] as $columnKey => $columnValue) {
+
+        switch ($columnKey) {
+          case "date":
+          // $tempArray[$rowKey]["date"] = $columnValue;
+          break;
+          case "row":
+          $tempArray[$rowKey]["row"] = $columnValue; 
+          
+          break;         
+          case "fmNo":
+          $tempArray[$rowKey]["fmNo"] = $columnValue;
+          
+          break;
+          case "activities":
+          $tempArray[$rowKey]["activities"] = $columnValue;                    
+            break;          
+          default:            
+            break;
+        }
+      }
+        $jsonIndex = array_search($resultData[$rowKey]["date"],array_column($jsonData,"date"));
+        // Append child Items array
+        $jsonData[$jsonIndex]["childTable"]["biA"]["items"][]=$tempArray[$rowKey];
+    }
+      
+    
+    
+  }
+
+  $tempArray = [];
+  $date = $data["date"];
+  $ptwFetch = $con -> prepare("SELECT * FROM `ptw` 
+  WHERE YEAR(`date`)=YEAR('$date') AND MONTH(`date`)=MONTH('$date')") ;
+
+  if($ptwFetch->execute()){
+    $resultData = $ptwFetch -> fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($resultData as $rowKey => $rowValue) {
+      foreach ($resultData[$rowKey] as $columnKey => $columnValue) {
+
+        switch ($columnKey) {
+          case "date":
+          // $tempArray[$rowKey]["date"] = $columnValue;
+          break;
+          case "row":
+          $tempArray[$rowKey]["row"] = $columnValue; 
+          
+          break;         
+          case "fmNo":
+          $tempArray[$rowKey]["fmNo"] = $columnValue;
+          
+          break;
+          case "activities":
+          $tempArray[$rowKey]["activities"] = $columnValue;                    
+            break;          
+          default:            
+            break;
+        }
+      }
+        $jsonIndex = array_search($resultData[$rowKey]["date"],array_column($jsonData,"date"));
+        // Append child Items array
+        $jsonData[$jsonIndex]["childTable"]["ptw"]["items"][]=$tempArray[$rowKey];
+    }
+      
+    
+    
+  }
+
+
+
+
+  $tempArray = [];
+  $date = $data["date"];
+  $paFetch = $con -> prepare("SELECT * FROM `pa` 
+  WHERE YEAR(`date`)=YEAR('$date') AND MONTH(`date`)=MONTH('$date')") ;
+
+  if($paFetch->execute()){
+    $resultData = $paFetch -> fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($resultData as $rowKey => $rowValue) {
+      foreach ($resultData[$rowKey] as $columnKey => $columnValue) {
+
+        switch ($columnKey) {
+          case "date":
+          // $tempArray[$rowKey]["date"] = $columnValue;
+          break;
+          case "row":
+          $tempArray[$rowKey]["row"] = $columnValue; 
+          
+          break;         
+          case "activities":
+          $tempArray[$rowKey]["activities"] = $columnValue;                    
+            break;          
+          default:            
+            break;
+        }
+      }
+        $jsonIndex = array_search($resultData[$rowKey]["date"],array_column($jsonData,"date"));
+        // Append child Items array
+        $jsonData[$jsonIndex]["childTable"]["pa"]["items"][]=$tempArray[$rowKey];
+    }
+      
+   
+    
+  }
+
+
+
+  $tempArray = [];
+  $date = $data["date"];
+  $cmFetch = $con -> prepare("SELECT * FROM `cm` 
+  WHERE YEAR(`date`)=YEAR('$date') AND MONTH(`date`)=MONTH('$date')") ;
+
+  if($cmFetch->execute()){
+    $resultData = $cmFetch -> fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($resultData as $rowKey => $rowValue) {
+      foreach ($resultData[$rowKey] as $columnKey => $columnValue) {
+
+        switch ($columnKey) {
+          case "date":
+          // $tempArray[$rowKey]["date"] = $columnValue;
+          break;
+          case "row":
+          $tempArray[$rowKey]["row"] = $columnValue; 
+          
+          break;         
+          case "activities":
+          $tempArray[$rowKey]["activities"] = $columnValue;                    
+            break;          
+          default:            
+            break;
+        }
+      }
+        $jsonIndex = array_search($resultData[$rowKey]["date"],array_column($jsonData,"date"));
+        // Append child Items array
+        $jsonData[$jsonIndex]["childTable"]["cm"]["items"][]=$tempArray[$rowKey];
+    }
+      
+   
+    
+  }
+
   echo json_encode($jsonData);
-}
+
+
 
 ?>
